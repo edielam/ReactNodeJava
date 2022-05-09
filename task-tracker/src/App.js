ce79bba1fddd53b78ad88e89d5eef8b4302ee9bb
edielam <edguard738@gmail.com>
2022-05-09 13:41:07 +0000

    props

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const name = " Ed"
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
-      <h2>Hello {1+1 + name}</h2>
+    <div className="container">
       <Header/>
     </div>
   );
--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
         <h1>Task Tracker</h1>
+        <Button color={'green'} text={'Hello'}/>
     </div>
   )
 }
