8075a551cabf5f67d8276f03369a3158af162eae
edielam <edguard738@gmail.com>
2022-05-10 18:27:01 +0000

    props

--- task-tracker/src/components/Button.js
+++ task-tracker/src/components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
   return (
     <button 
+        onClick={onClick}
         className='btn' 
         style={{backgroundColor: color}}>
         {text}
--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Button from './Button'
+import PropTypes from 'prop-types'
 
-const Header = () => {
-  return (
-    <div className='header'>
-        <h1>Task Tracker</h1>
-        <Button color={'green'} text={'Hello'}/>
-    </div>
+const Header = ({title}) => {
+    const onClick = () =>{
+        console.log("Hey!")
+    }
+    return (
+        <div className='header'>
+            <h1>{title}</h1>
+            <Button color={'green'} text={'Add'} onClick={onClick}/>
+        </div>
   )
+}
+Header.defaultProps ={
+    title: 'Task tracker',
+}
+// using proptypes helps make code more robust
+//for catching some errors
+Header.propTypes ={
+    title: PropTypes.string,
 }
 
 export default Header
