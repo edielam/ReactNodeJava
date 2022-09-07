f4f87a199a0ae74b2bbdf295e76648e979adc0fd
edielam <edguard738@gmail.com>
2022-09-07 20:25:50 +0000

    simple mern

--- simplemern/server/server.js
+++ simplemern/server/server.js
@@ -12,3 +12,11 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
 app.get('/', (req, res) => {
     res.status(200).send(template())
     })
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+    if (err) {
+    console.log(err)
+    }
+    console.info('Server started on port %s.', port)
+})
