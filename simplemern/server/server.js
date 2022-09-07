d6561b0fa11378287be793b9cc0b70ce69940ec4
edielam <edguard738@gmail.com>
2022-09-07 20:24:43 +0000

    simple mern

--- simplemern/server/server.js
+++ simplemern/server/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import devBundle from './devBundle'
 import path from 'path'
+import template from './../template'
 
 const CURRENT_WORKING_DIR = process.cwd()
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
-'dist')))
 
 const app = express()
 devBundle.compile(app)
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
+    'dist')))
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+    })
