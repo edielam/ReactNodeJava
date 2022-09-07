b5cfc0749243e4c4309905d7dd1073965d05c0e9
edielam <edguard738@gmail.com>
2022-09-07 20:22:35 +0000

    simple mern

--- simplemern/server/server.js
+++ simplemern/server/server.js
@@ -1,5 +1,10 @@
 import express from 'express'
 import devBundle from './devBundle'
+import path from 'path'
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
+'dist')))
 
 const app = express()
 devBundle.compile(app)
