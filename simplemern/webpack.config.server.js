0c91c5176126e854db4111fe3fd9b12b11b8cf5b
edielam <edguard738@gmail.com>
2022-09-07 18:42:30 +0000

    simple mern

--- simplemern/webpack.config.server.js
+++ simplemern/webpack.config.server.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const CURRENT_WORKING_DIR = process.cwd()
-
+// const path = require('path');
+// const webpack = require('webpack');
+// const CURRENT_WORKING_DIR = process.cwd()
+cosnt nodeExternals =
 const config = {}
 
 module.exports = config;
