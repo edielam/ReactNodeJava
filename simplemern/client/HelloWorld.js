39997b292befd50a360f601a34662481af2fe00b
edielam <edguard738@gmail.com>
2022-09-07 19:39:19 +0000

    simple mern

--- simplemern/client/HelloWorld.js
+++ simplemern/client/HelloWorld.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import {hot} froom 'react-hot-loader'
 
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
 
+export default hot(module)(HelloWorld)
