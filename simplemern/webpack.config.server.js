07124ee487a9cf1c2559b52fe5920097679b6e6f
edielam <edguard738@gmail.com>
2022-09-07 19:13:07 +0000

    simple mern

--- simplemern/webpack.config.server.js
+++ simplemern/webpack.config.server.js
@@ -1,7 +1,7 @@
-// const path = require('path');
-// const webpack = require('webpack');
-// const CURRENT_WORKING_DIR = process.cwd()
-cosnt nodeExternals = require('webpack-node-externals')
+const path = require('path');
+cosnt nodeExternals = require('webpack-node-externals');
+const CURRENT_WORKING_DIR = process.cwd()
+
 const config = {
     name: "server",
     entry: [ path.join(CURRENT_WORKING_DIR , './server/server.js') ],
