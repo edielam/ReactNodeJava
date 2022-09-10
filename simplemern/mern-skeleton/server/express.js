464a621b462c3946d9fcc8fb77f4f3aa6f3dec8e
edielam <edguard738@gmail.com>
2022-09-10 10:39:48 +0000

    skeleton

--- simplemern/mern-skeleton/server/express.js
+++ simplemern/mern-skeleton/server/express.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded{extended: true});
 app.use(cookieParser());
 app.use(compress());
+app.use(helmet());
+app.use(cors());
 
 
 export default app;
