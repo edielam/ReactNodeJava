8c6912e460e5cb0d13c366ae904ad8731317bf2c
edielam <edguard738@gmail.com>
2022-09-10 17:20:20 +0000

    serving template

--- simplemern/mern-skeleton/server/models/user.model.js
+++ simplemern/mern-skeleton/server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import crypto from 'crypto'
 
 const UserSkema = new mongoose.Schema({
     name: {
@@ -37,4 +38,26 @@ UserSkema
         return this._password
     })
 
-UserSkema.path('hashedPassword').validate()
+UserSkema.path('hashedPassword').validate(function(e){
+    if (this._password && this._password.length < 8){
+        this.invalidate('password', 'Length of passowrd must be at least 8 characters')
+    }
+    if (this.isNew && !this._password){
+        this.invalidate('passowrd', "Please enter your passowrd")
+    }
+}, null)
+
+UserSkema.methods = {
+    authenticate: function(plainText){
+        return this.encryptPassowrd(plainText) === this.hashedPasssword
+    },
+    encryptPassowrd: function(passowrd){
+        if(!passowrd) return ''
+        try {
+            return crypto
+            .cre
+        } catch (error) {
+            
+        }
+    }
+}
