81bbe9c52ac683bf0bb78115efd83010db05f7cc
edielam <edguard738@gmail.com>
2022-09-10 10:50:20 +0000

    skeleton

--- simplemern/mern-skeleton/server/express.js
+++ simplemern/mern-skeleton/server/express.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded{extended: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
--- simplemern/mern-skeleton/server/server.js
+++ simplemern/mern-skeleton/server/server.js
@@ -1,4 +1,5 @@
 import config from "../config/config";
+import mongoose from 'mongoose';
 import app from "./express";
 
 app.listen(config.port, (err) => {
@@ -6,4 +7,12 @@ app.listen(config.port, (err) => {
         console.log(err)
     }
     console.info('Server started on port %s.', config.port)
+})
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true } )
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${mongoUri}`)
 })
