8b58cc7c6ad6f4c570d26d4c0b4242d929eeb434
edielam <edguard738@gmail.com>
2022-09-10 16:45:40 +0000

    serving template

--- simplemern/mern-skeleton/server/models/user.model.js
+++ simplemern/mern-skeleton/server/models/user.model.js
@@ -5,6 +5,21 @@ const UserSkema = new mongoose.Schema({
         type: String,
         trim: true,
         required: 'Please enter your name'
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: "Email already exits",
+        match:[/.+\@.+\..+/, "Enter a valid email address"],
+        required: 'Please enter your email'
+
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date,
+    hashedPasssword: {
+        
     }
-    
 })
