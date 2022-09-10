c1c5f121f546d88c21cd372e7db5721dc11cb38d
edielam <edguard738@gmail.com>
2022-09-10 10:38:50 +0000

    skeleton

--- simplemern/mern-skeleton/server/express.js
+++ simplemern/mern-skeleton/server/express.js
@@ -1,5 +1,16 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded{extended: true});
+app.use(cookieParser());
+app.use(compress());
+
 
 export default app;
