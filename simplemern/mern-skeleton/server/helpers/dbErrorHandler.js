17da7c0f7974668b17239818e2908774c6cc90ab
edielam <edguard738@gmail.com>
2022-09-10 18:20:28 +0000

    serving template

--- simplemern/mern-skeleton/server/helpers/dbErrorHandler.js
+++ simplemern/mern-skeleton/server/helpers/dbErrorHandler.js
@@ -5,6 +5,18 @@ const getErrorMessage = (err) => {
         switch (err.code){
             case 11000:
             case 11001:
+                message = getUniqueErrorMessage(err)
+                break
+            default:
+                message = 'There is a problem'
         }
     }
+    else {
+        for (let errorMesg in err.errors){
+            if (err.errors[errorMesg].message) message = err.errors[errorMesg].message
+        }
+    }
+    return message
 }
+
+export default {getErrorMessage}
